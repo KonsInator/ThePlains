a520759558eb6d0ada485a7d03ddc1a477ef4050
Konstantin Kochs <konstantin@flammiger.info>
2022-03-19 17:59:38 +0100

    GameObject-Klasse begonen

---
+++ src/main/java/gameobjects/GameObject.java
@@ -0,0 +1,15 @@
+package gameobjects;
+
+import javax.swing.*;
+
+public abstract class GameObject {
+    private ImageIcon texture;
+
+    public ImageIcon getTexture() {
+        return texture;
+    }
+
+    public void setTexture(ImageIcon texture) {
+        this.texture = texture;
+    }
+}
