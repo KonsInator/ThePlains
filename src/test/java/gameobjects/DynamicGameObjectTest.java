5182e9d7ddaa0ade31b557ff28cc04b1590f23e4
Konstantin Kochs <konstantin@flammiger.info>
2022-03-19 20:53:54 +0100

    hasId(id) getestet

---
+++ src/test/java/gameobjects/DynamicGameObjectTest.java
@@ -0,0 +1,32 @@
+package gameobjects;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+class DynamicGameObjectTest {
+
+    private DynamicGameObject o1;
+    private DynamicGameObject o2;
+
+    private DynamicPosition pos1;
+    private DynamicPosition pos2;
+
+    @BeforeEach
+    public void setUp() {
+        pos1 = mock(DynamicPosition.class);
+        pos1 = mock(DynamicPosition.class);
+
+        o1 = new DynamicGameObject(pos1);
+        o2 = new DynamicGameObject(pos2);
+    }
+
+    @Test
+    public void testHasId() {
+        assertTrue(o1.hasId(o1.getId()));
+        assertFalse(o1.hasId(o2.getId()));
+    }
+
+}
