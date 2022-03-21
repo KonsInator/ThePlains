fed2e9eaee91767d57f9453d008b34dfa82c7854
Konstantin Kochs <konstantin@flammiger.info>
2022-03-21 13:19:31 +0100

    ObjectId hat long-Wert und ist java.lang.Number

--- src/main/java/gameobjects/ObjectId.java
+++ src/main/java/gameobjects/ObjectId.java
@@ -5,14 +5,64 @@ import java.util.concurrent.atomic.AtomicLong;
 /**
  * unique ID for {@link GameObject}s based on {@link AtomicLong}
  */
-public class ObjectId extends AtomicLong {
+public class ObjectId extends Number {
     private static AtomicLong idCounter = new AtomicLong();
 
     public static ObjectId getUniqueId() {
         return new ObjectId(idCounter.getAndIncrement());
     }
 
-    private ObjectId(long id) {
-        this.set(id);
+    private final long value;
+
+    private ObjectId(long value) {
+        this.value = value;
+    }
+
+    /**
+     * Method that should be used to get the actual long {@link #value} of the {@link ObjectId}
+     * @return the actual long value
+     */
+    public long get() {
+        return value;
+    }
+
+    /**
+     * {@inheritDoc}
+     * To get the actual {@link #value} in {@link long} use {@link #get()} instead!
+     * @return {@link #value} cast to {@link int}
+     */
+    @Override
+    public int intValue() {
+        return (int)value;
+    }
+
+    /**
+     * {@inheritDoc}
+     * To get the actual {@link #value} use {@link #get()} instead!
+     * @return
+     */
+    @Override
+    public long longValue() {
+        return value;
+    }
+
+    /**
+     * {@inheritDoc}
+     * To get the actual {@link #value} in {@link long} use {@link #get()} instead!
+     * @return {@link #value} cast to {@link float}
+     */
+    @Override
+    public float floatValue() {
+        return (float)value;
+    }
+
+    /**
+     * {@inheritDoc}
+     * To get the actual {@link #value} in {@link long} use {@link #get()} instead!
+     * @return {@link #value} cast to {@link double}
+     */
+    @Override
+    public double doubleValue() {
+        return (double)value;
     }
 }
--- src/test/java/gameobjects/ObjectIdTest.java
+++ src/test/java/gameobjects/ObjectIdTest.java
@@ -8,11 +8,11 @@ class ObjectIdTest {
 
     @Test
     void getUniqueId() {
-        assertEquals(0L, ObjectId.getUniqueId().longValue());
-        assertEquals(1L, ObjectId.getUniqueId().longValue());
-        assertEquals(2L, ObjectId.getUniqueId().longValue());
-        assertEquals(3L, ObjectId.getUniqueId().longValue());
-        assertEquals(4L, ObjectId.getUniqueId().longValue());
-        assertEquals(5L, ObjectId.getUniqueId().longValue());
+        assertEquals(0L, ObjectId.getUniqueId().get());
+        assertEquals(1L, ObjectId.getUniqueId().get());
+        assertEquals(2L, ObjectId.getUniqueId().get());
+        assertEquals(3L, ObjectId.getUniqueId().get());
+        assertEquals(4L, ObjectId.getUniqueId().get());
+        assertEquals(5L, ObjectId.getUniqueId().get());
     }
 }
