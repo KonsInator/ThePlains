5be2025d647161e2e7d9b2ac7db30ab33c227905
Konstantin Kochs <konstantin@flammiger.info>
2022-03-22 10:43:32 +0100

    GameObjectCollection bereinigt

--- src/main/java/com/theplains/gameobjects/GameObjectCollection.java
+++ src/main/java/com/theplains/gameobjects/GameObjectCollection.java
@@ -6,20 +6,20 @@ import java.util.Optional;
 
 /**
  * Collection of {@link GameObject}s
- * @param <GO>
+ * @param <O>
  */
-public class GameObjectCollection<GO extends GameObject> extends HashSet<GO> {
+public class GameObjectCollection<O extends GameObject> extends HashSet<O> {
 
-    public GameObjectCollection(Collection<? extends GO> c) {
+    public GameObjectCollection(Collection<? extends O> c) {
         super(c);
     }
 
     /**
-     * get a {@link GO} out of Collection with given id
-     * @return {@link GO} with id or {@literal null} if there is no object with given id in collection
+     * get a {@link O} out of Collection with given id
+     * @return {@link O} with id or {@literal null} if there is no object with given id in collection
      */
-    public Optional<GO> getById(ObjectId id) {
-        for (GO o : this) {
+    public Optional<O> getById(ObjectId id) {
+        for (O o : this) {
            if (o.hasId(id)) {
                return Optional.of(o);
            }
@@ -28,7 +28,7 @@ public class GameObjectCollection<GO extends GameObject> extends HashSet<GO> {
     }
 
     /**
-     * remove a {@link GO} out of collection with given id
+     * remove a {@link O} out of collection with given id
      * @return {@literal true} if an element has been removed
      */
     public boolean removeById(ObjectId id) {
@@ -36,11 +36,11 @@ public class GameObjectCollection<GO extends GameObject> extends HashSet<GO> {
     }
 
     /**
-     * remove a {@link GO} out of collection with given id
-     * @return the removed element in {@link Optional<GO>}
+     * remove a {@link O} out of collection with given id
+     * @return the removed element in {@link Optional< O >}
      */
-    public Optional<GO> removeAndGetById(ObjectId id) {
-        for (GO o : this) {
+    public Optional<O> removeAndGetById(ObjectId id) {
+        for (O o : this) {
             if (o.hasId(id)) {
                 this.remove(o);
                 return Optional.of(o);
